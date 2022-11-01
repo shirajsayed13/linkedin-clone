05ef5cac1c476004b14e330c0f632c758542073e
shirajsayed <shirajsayed13@gmail.com>
2022-11-01 15:06:22 +0530

    Add post and firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Sidebar from './Sidebar'
+import Feed from "./Feed"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Lets build a LinkedIn Clone</h1>
+    <div className="app">
+      {/* Header */}
+      <Header />
+      {/* App Body */}
+      <div className="app__body">
+        <Sidebar />
+        <Feed/>
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/firebaseLocal.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD79tNYwqZd6yO1RI4WkH19CJfsKRkKPy8",
+    authDomain: "linkedin-clone-636a9.firebaseapp.com",
+    projectId: "linkedin-clone-636a9",
+    storageBucket: "linkedin-clone-636a9.appspot.com",
+    messagingSenderId: "539870555323",
+    appId: "1:539870555323:web:33ea4966e34a751f2c4d9e"
+  };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+
+export default db;
